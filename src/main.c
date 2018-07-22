e5040538587cb09a0cd21ca19bfb945b2bb7ea0d
Brent <>
2018-07-22 13:09:45 -0700

    added in provided code

--- src/main.c
+++ src/main.c
@@ -40,6 +40,17 @@
 #include "stm32f4xx_hal.h"
 #include <string.h> //for memcpy
 
+#include <hamming.h>
+#include "stm32f4xx_hal.h"
+#include "math.h"
+
+#include "arm_math.h"
+#include "arm_const_structs.h"
+#include "main.h"
+#include "hamming.h"
+#include "windowing_fft.h"
+#include "AudioChip.h"
+
 // TODO move #define to header
 
 // ----------------------------------------------------------------------------
@@ -139,6 +150,22 @@
 #define ENABLE_ECHO				(1)
 #define ENABLE_PITCH_SHIFT		(1)
 
+#define SEGMENT_A 				( GPIO_PIN_0 )
+#define SEGMENT_B				( GPIO_PIN_0 )
+#define SEGMENT_C				( GPIO_PIN_0 )
+#define SEGMENT_D				( GPIO_PIN_0 )
+#define SEGMENT_E				( GPIO_PIN_0 )
+#define SEGMENT_F				( GPIO_PIN_0 )
+#define SEGMENT_G				( GPIO_PIN_0 )
+
+#define LED_DIGIT_1 			( GPIO_PIN_0 )
+#define LED_DIGIT_2				( GPIO_PIN_0 )
+#define LED_DIGIT_3				( GPIO_PIN_0 )
+#define LED_DIGIT_4				( GPIO_PIN_0 )
+
+#define SWITCH_1				( GPIO_PIN_0 )
+#define SWITCH_2				( GPIO_PIN_0 )
+
 volatile char previous_button_reading_PA0 = 0;
 volatile char button_state_PA0 = 0;
 volatile char previous_button_reading_PC1 = 0;
@@ -166,6 +193,91 @@ const int times_to_repeat_frame = REFRESH_RATE / FRAMES_PER_SECOND;
 int LED_Array_State = 0; //TODO remove after functional test demo
 int pitch_shift_state = NO_EFFECT;
 int echo_state = NO_EFFECT;
+
+
+
+uint16_t
+	LedBuffer[4] = {};
+
+
+volatile uint16_t
+	LedRefreshCount = 0,
+	LedDisplayedDigit = 0;
+
+volatile int16_t
+	EchoBuffer[16384];
+
+volatile uint16_t
+	EchoPointer = 0;
+
+volatile uint8_t
+	ClearEchoBuffer = TRUE;
+
+//
+// Data structure for timer configuration
+//
+
+TIM_HandleTypeDef
+	Timer5_16Khz;
+
+//
+// Data structure for general purpose IO configuration
+//
+
+GPIO_InitTypeDef
+	GpioInitStructure;
+
+//
+// Data structure for the D/A(DAC) Converter configuration
+//
+
+DAC_ChannelConfTypeDef
+	DacInitStructure;
+
+DAC_HandleTypeDef
+	AudioDac;				// Structure for the audio digital to analog converter subsystem
+
+//
+// Data structures for the A/D Converter configuration
+//
+
+ADC_HandleTypeDef
+	AudioAdc,
+	ReferenceAdc;
+
+
+volatile int
+	ButtonCount = 0,
+	ButtonState = RELEASED,
+	Effect = NO_EFFECT;
+
+
+//
+// Buffering system variables
+//
+
+volatile int
+	ADCPTR = 0;
+
+volatile struct tBuffer
+	Buffers[NUMBER_OF_BUFFERS];
+
+
+volatile int
+	WindowingState = 0,
+	WindowingDone = FALSE;
+
+//
+// 4 times the size of the main buffer to compensate for addition of complex numbers and that we are processing
+// 2 buffers at a time
+//
+
+float
+	delayedBuf[SIZE*4],
+	procBuf[SIZE*4];
+
+int
+	AD_Offset;
 
 void Init_GPIO_Port(uint32_t pin, uint32_t mode, uint32_t speed, uint32_t pull, char bus)
 {
@@ -602,11 +714,336 @@ void Update_State()
 	}
 }
 
+
+
+/*
+ * Name: UpdateLedDisplay
+ *
+ * Description: Update a specific seven segment digit on the 4 digit display
+ *
+ * Inputs:
+ * 		None
+ *
+ * Output:
+ * 		None
+ *
+ * Process:
+ *
+ * 		Display the selected digit and advance to the next digit
+ * 		Refresh the display at a rate with no flicker
+ *
+ */
+
+void UpdateLedDisplay( void )
+{
+
+}
+
+/*
+ * Name: TIM5_IRQHandler
+ *
+ * Description: Time 5 interrupt service routine call 16,000 times a second.
+ * Inputs:
+ * 		None
+ *
+ * Output:
+ * 		None
+ *
+ * Process:
+ *
+ * 		Send audio signal to D/A converter
+ *		Sample audio input
+ *		Do echoing effect
+ *		Handle windowing state update
+ *		Update the LED display
+ *		Detect button press and remove bounce
+ *		Switch effects mode
+ *
+ */
+
+
+void TIM5_IRQHandler(void)
+{
+
+	int16_t
+		AudioSignal;
+
+	TIMER_DEBUG_SIGNAL_ON;
+
+
+//
+// Check for timer update interrupt
+//
+	if ( __HAL_TIM_GET_FLAG( &Timer5_16Khz, TIM_IT_UPDATE ) != RESET )
+	{
+
+
+//
+// Check for buffer full status
+//
+		if( 3 == Buffers[ANALOG_OUT_OFFSET].Full )
+		{
+//
+// Output the Audio stream to the D/A converter
+//
+			DAC -> DHR12R1 = Buffers[ANALOG_OUT_OFFSET].Buf[Buffers[ANALOG_OUT_OFFSET].Head];
+
+//
+// Advanced the head pointer and check for end of buffer
+//
+			Buffers[ANALOG_OUT_OFFSET].Head++;		//increment head
+
+			if( Buffers[ANALOG_OUT_OFFSET].Head >= SIZE)
+			{
+//
+// Set the head pointer to the start of the buffer
+// Reset the buffer full status
+//
+				Buffers[ANALOG_OUT_OFFSET].Head = 0;
+				Buffers[ANALOG_OUT_OFFSET].Full = 0;
+			}
+		}
+
+
+//
+// Get values from adc and fill the buffer. when it is full reset the
+// head pointer and set status to full then increment ALL buffers
+// the & 0x03 is to loop the buffers back to 0 when they get to 4
+// the << 3 is to increase the volume due to only being a 12b adc
+//
+
+//
+// See if the buffer is not full
+//
+		if( 0  == Buffers[ADCPTR].Full)
+		{
+
+//
+// Take a reading of the analog input pin and remove the offset signal
+//
+			AudioSignal = HAL_ADC_GetValue( &AudioAdc ) - AD_Offset;
+
+//
+// If enabled do the echo effect on the raw signal
+//
+			if ( ECHO == Effect )
+			{
+
+			}
+			else
+			{
+
+//
+// No echo effect. just store the data in the buffer
+//
+
+				Buffers[ADCPTR].Buf[Buffers[ADCPTR].Head] = AudioSignal;
+			}
+
+//
+// Update the head pointer
+//
+			Buffers[ADCPTR].Head++;
+
+//
+// See if the buffer is full
+//
+			if( Buffers[ADCPTR].Head >= SIZE )
+			{
+
+//
+// If this statement returns true then the FFT portion of the code has failed.
+//
+				if (( FALSE == WindowingDone ) && ( 0 != WindowingState ))
+				{
+//
+// Fatal error
+//
+					while ( TRUE );
+				}
+
+//
+// Advance to the next buffer
+//
+				Buffers[ADCPTR].Head = 0;		// Reset the head pointer
+				Buffers[ADCPTR].Full = 1;		// Buffer Full = 1
+				ADCPTR = ( ADCPTR + 1 ) & BUFFERS_MASK;
+
+//
+// changes the state for the overlapping windowing system
+//
+				switch( WindowingState )
+				{
+					case 0:
+					{
+						WindowingState = 1;
+						WindowingDone = FALSE;
+						break;
+					}
+
+					case 1:
+					{
+						WindowingState = 2;
+						WindowingDone = FALSE;
+						break;
+					}
+
+					case 2:
+					{
+						WindowingState = 3;
+						WindowingDone = FALSE;
+						break;
+					}
+
+					case 3:
+					{
+						WindowingState = 4;
+						WindowingDone = FALSE;
+						break;
+					}
+
+					case 4:
+					{
+						WindowingState = 3;
+						WindowingDone = FALSE;
+						break;
+					}
+
+					default:
+					{
+
+//
+// Invalid state. Should not get here
+//
+						while ( TRUE );
+						break;
+					}
+				}
+			}
+		}
+
+//
+// Start another conversion
+//
+		HAL_ADC_Start( &AudioAdc );
+
+//
+// Update the multiplexing LED display
+//
+
+		UpdateLedDisplay();
+
+//
+// Button push detection with debounce
+//
+		if ( HAL_GPIO_ReadPin( GPIOD, SWITCH_1 ) == GPIO_PIN_RESET )
+		{
+
+		}
+		else
+		{
+
+		}
+
+
+
+//
+// Clear the timer update interrupt flag
+//
+		__HAL_TIM_CLEAR_FLAG( &Timer5_16Khz, TIM_IT_UPDATE );
+
+	}
+	TIMER_DEBUG_SIGNAL_OFF;
+}
+
+
+void PitchShift( float *Buffer )
+{
+	int
+		PitchShift,
+//
+// Pitch Shift by 32 bins in the FFT table
+// Each bin contains one complex number comprised of one real and one imaginary floating point number
+//
+	PitchOffset = 32 * 2;
+
+
+	if (( PITCH_SHIFT_UP_8 == Effect ) || ( PITCH_SHIFT_DOWN_8 == Effect ))
+	{
+		PitchOffset = 8 * 2;
+	}
+
+	if (( PITCH_SHIFT_UP_16 == Effect ) || ( PITCH_SHIFT_DOWN_16 == Effect ))
+	{
+		PitchOffset = 16 * 2;
+	}
+
+//
+// Shift frequencies up effect
+//
+
+	if (( PITCH_SHIFT_UP_8 == Effect ) || ( PITCH_SHIFT_UP_16 == Effect ) || ( PITCH_SHIFT_UP_32 == Effect ))
+	{
+//
+// Do the lower half of FFT table
+//
+		PitchShift = 1022;
+		while ( PitchShift >= PitchOffset  )
+		{
+			Buffer[PitchShift] = Buffer[PitchShift-PitchOffset];
+			Buffer[PitchShift+1] = Buffer[(PitchShift+1)-PitchOffset];
+			PitchShift -= 2;
+		}
+
+//
+// Clear the duplicated portion of the table
+//
+		while ( PitchShift >= 0 )
+		{
+			Buffer[PitchShift] = 0;
+			PitchShift--;
+		}
+
+
+//
+// Do the upper half of the FFT table
+//
+		PitchShift = 1024;
+		while ( PitchShift < ( 2048 - PitchOffset ))
+		{
+			Buffer[PitchShift] = Buffer[PitchShift+PitchOffset];
+			Buffer[PitchShift+1] = Buffer[(PitchShift+1)+PitchOffset];
+			PitchShift += 2;
+		}
+//
+// Clear the duplicated portion of the table
+//
+		while ( PitchShift < 2048 )
+		{
+			Buffer[PitchShift] = 0;
+			PitchShift++;
+		}
+	}
+
+
+//
+// Shift frequencies down effect
+//
+
+	if (( PITCH_SHIFT_DOWN_8 == Effect ) || ( PITCH_SHIFT_DOWN_16 == Effect ) || ( PITCH_SHIFT_DOWN_32 == Effect ))
+	{
+
+
+	}
+}
+
 int
 main(int argc, char* argv[])
 {
   // At this stage the system clock should have already been configured
   // at high speed.
+
+	unsigned int loop;
 
 	HAL_Init();// initializing HAL drivers
 
@@ -615,6 +1052,15 @@ main(int argc, char* argv[])
 	__GPIOC_CLK_ENABLE(); // enabling clock for port C
 	__GPIOD_CLK_ENABLE(); // enabling clock for port D
 	__GPIOE_CLK_ENABLE(); // enabling clock for port E
+
+	for( loop = 0; loop < NUMBER_OF_BUFFERS; loop++)
+	{
+		Buffers[loop].Head = 0;
+		Buffers[loop].Full = 0;
+		memset( (_PTR)&Buffers[loop].Buf, 0, sizeof( Buffers[loop].Buf ));
+	}
+
+	InitSystemPeripherals();
 
 	Configure_Ports();
 	Configure_LED_Display();
@@ -636,6 +1082,13 @@ main(int argc, char* argv[])
 
 	int column = 0;
 	int height = 3;
+
+//
+//	Take an Offset reading to remove the DC offset from the analog reading
+//  Source PC2 ( ADC_CHANNEL_12 )
+//
+	AD_Offset = ConvertReference();
+
 	// Infinite loop
 	while (1)
 	{
@@ -655,6 +1108,8 @@ main(int argc, char* argv[])
 		}
 
 		Update_State();
+
+		WindowingFFT();
 	}
 	//TODO add code upon stop execution of program
 }
@@ -854,6 +1309,248 @@ void TIM4_IRQHandler() //Timer4 interrupt function
 	}
 
 	current_row++; //move to next row
+}
+
+void InitSystemPeripherals( void )
+{
+
+	ADC_ChannelConfTypeDef
+		sConfig;
+
+//
+// Enable device clocks TIMER and GPIO port E
+//
+	__HAL_RCC_TIM5_CLK_ENABLE();
+	__HAL_RCC_GPIOE_CLK_ENABLE();
+	__HAL_RCC_DAC_CLK_ENABLE();
+
+
+//
+// Enable ADC3 and GPIO port C clocks
+//
+	__HAL_RCC_ADC1_CLK_ENABLE();
+	__HAL_RCC_ADC2_CLK_ENABLE();
+	__HAL_RCC_GPIOC_CLK_ENABLE();
+	__HAL_RCC_GPIOA_CLK_ENABLE();
+	__HAL_RCC_GPIOD_CLK_ENABLE();
+	__HAL_RCC_GPIOB_CLK_ENABLE();
+
+	GpioInitStructure.Pin = SEGMENT_A | SEGMENT_B | SEGMENT_C | SEGMENT_D | SEGMENT_E | SEGMENT_F | SEGMENT_G;
+	GpioInitStructure.Mode = GPIO_MODE_OUTPUT_PP;
+	GpioInitStructure.Speed = GPIO_SPEED_FREQ_MEDIUM;
+	GpioInitStructure.Pull = GPIO_PULLUP;
+	GpioInitStructure.Alternate = 0;
+	HAL_GPIO_Init(GPIOD, &GpioInitStructure );
+
+	GpioInitStructure.Pin = LED_DIGIT_1 | LED_DIGIT_2 | LED_DIGIT_3 | LED_DIGIT_4;
+	GpioInitStructure.Mode = GPIO_MODE_OUTPUT_PP;
+	GpioInitStructure.Speed = GPIO_SPEED_FREQ_MEDIUM;
+	GpioInitStructure.Pull = GPIO_PULLUP;
+	GpioInitStructure.Alternate = 0;
+	HAL_GPIO_Init(GPIOB, &GpioInitStructure );
+
+
+
+
+//
+// Enable GPIO Port E8 as an input ( used for button select options )
+//
+
+	GpioInitStructure.Pin = SWITCH_1 | SWITCH_2;
+	GpioInitStructure.Mode = GPIO_MODE_INPUT;
+	GpioInitStructure.Speed = GPIO_SPEED_FREQ_MEDIUM;
+	GpioInitStructure.Pull = GPIO_PULLUP;
+	GpioInitStructure.Alternate = 0;
+	HAL_GPIO_Init(GPIOD, &GpioInitStructure );
+
+//
+// Enable GPIO Port E15 as an output ( used for timing with scope )
+//
+
+//	GpioInitStructure.Pin = GPIO_PIN_15 | GPIO_PIN_13;
+//	GpioInitStructure.Mode = GPIO_MODE_OUTPUT_PP;
+//	GpioInitStructure.Speed = GPIO_SPEED_FREQ_MEDIUM;
+//	GpioInitStructure.Pull = GPIO_PULLUP;
+//	GpioInitStructure.Alternate = 0;
+//	HAL_GPIO_Init(GPIOD, &GpioInitStructure);
+
+//
+// Enable GPIO port A1 as an analog output
+//
+	GpioInitStructure.Pin = GPIO_PIN_4;
+	GpioInitStructure.Mode = GPIO_MODE_ANALOG;
+	GpioInitStructure.Speed = GPIO_SPEED_FREQ_MEDIUM;
+	GpioInitStructure.Pull = GPIO_NOPULL;
+	GpioInitStructure.Alternate = 0;
+	HAL_GPIO_Init(GPIOA, &GpioInitStructure );
+
+	EnableAudioCodecPassThru();
+
+//
+// Configure DAC channel 1
+//
+	AudioDac.Instance = DAC;
+
+	HAL_DAC_Init( &AudioDac );
+
+	DacInitStructure.DAC_Trigger = DAC_TRIGGER_NONE;
+	DacInitStructure.DAC_OutputBuffer = DAC_OUTPUTBUFFER_ENABLE;
+	HAL_DAC_ConfigChannel( &AudioDac, &DacInitStructure ,DAC_CHANNEL_1 );
+
+
+//
+// Enable DAC channel 1
+//
+//
+	HAL_DAC_Start( &AudioDac, DAC_CHANNEL_1 );
+
+//
+// Configure A/D converter channel 3
+//
+
+//
+// Enable GPIO port C1, C2 and C5 as an analog input
+//
+	GpioInitStructure.Pin = GPIO_PIN_1 | GPIO_PIN_2 | GPIO_PIN_5;
+	GpioInitStructure.Mode = GPIO_MODE_ANALOG;
+	GpioInitStructure.Speed = GPIO_SPEED_FREQ_MEDIUM;
+	GpioInitStructure.Pull = GPIO_NOPULL;
+	GpioInitStructure.Alternate = 0;
+	HAL_GPIO_Init(GPIOC, &GpioInitStructure );
+
+//
+// Configure audio A/D ( ADC2 ) for the audio stream
+//
+
+	AudioAdc.Instance = ADC2;
+	AudioAdc.Init.ClockPrescaler = ADC_CLOCKPRESCALER_PCLK_DIV2;
+	AudioAdc.Init.Resolution = ADC_RESOLUTION_12B;
+	AudioAdc.Init.ScanConvMode = DISABLE;
+	AudioAdc.Init.ContinuousConvMode = DISABLE;
+	AudioAdc.Init.DiscontinuousConvMode = DISABLE;
+	AudioAdc.Init.NbrOfDiscConversion = 0;
+	AudioAdc.Init.ExternalTrigConv = ADC_EXTERNALTRIGCONV_T1_CC1;
+	AudioAdc.Init.ExternalTrigConvEdge = ADC_EXTERNALTRIGCONVEDGE_NONE;
+	AudioAdc.Init.NbrOfConversion = 1;
+	AudioAdc.Init.DataAlign = ADC_DATAALIGN_RIGHT;
+	AudioAdc.Init.DMAContinuousRequests = DISABLE;
+	AudioAdc.Init.EOCSelection = ADC_EOC_SINGLE_CONV;
+	HAL_ADC_Init( &AudioAdc );
+
+//
+// Select PORTC pin 5 ( ADC_CHANNEL_15 ) for the audio stream
+//
+	sConfig.Channel = ADC_CHANNEL_15;
+//	sConfig.Channel = ADC_CHANNEL_11;
+	sConfig.Rank = 1;
+	sConfig.SamplingTime = ADC_SAMPLETIME_112CYCLES;
+	sConfig.Offset = 0;
+
+	HAL_ADC_ConfigChannel(&AudioAdc, &sConfig);
+	HAL_ADC_Start( &AudioAdc );
+
+//
+// Configure level shifting reference A/D (ADC1)
+//
+	ReferenceAdc.Instance = ADC1;
+	ReferenceAdc.Init.ClockPrescaler = ADC_CLOCKPRESCALER_PCLK_DIV2;
+	ReferenceAdc.Init.Resolution = ADC_RESOLUTION_12B;
+	ReferenceAdc.Init.ScanConvMode = DISABLE;
+	ReferenceAdc.Init.ContinuousConvMode = DISABLE;
+	ReferenceAdc.Init.DiscontinuousConvMode = DISABLE;
+	ReferenceAdc.Init.ExternalTrigConv = ADC_EXTERNALTRIGCONV_T1_CC1;
+	ReferenceAdc.Init.ExternalTrigConvEdge = ADC_EXTERNALTRIGCONVEDGE_NONE;
+	ReferenceAdc.Init.NbrOfConversion = 1;
+	ReferenceAdc.Init.NbrOfDiscConversion = 0;
+	ReferenceAdc.Init.DataAlign = ADC_DATAALIGN_RIGHT;
+	ReferenceAdc.Init.DMAContinuousRequests = DISABLE;
+	ReferenceAdc.Init.EOCSelection = ADC_EOC_SINGLE_CONV;
+	HAL_ADC_Init( &ReferenceAdc );
+	HAL_ADC_Start( &ReferenceAdc );
+
+//
+// Initialize timer to 16Khz
+//
+	Timer5_16Khz.Instance = TIM5;
+	Timer5_16Khz.Init.CounterMode = TIM_COUNTERMODE_UP;
+	Timer5_16Khz.Init.Period = 250;
+	Timer5_16Khz.Init.Prescaler = 20;
+	Timer5_16Khz.Init.ClockDivision = TIM_CLOCKDIVISION_DIV1;
+	HAL_TIM_Base_Init( &Timer5_16Khz );
+
+//
+// Enable the timer interrupt
+//
+	HAL_NVIC_SetPriority( TIM5_IRQn, 0, 0);
+	HAL_NVIC_EnableIRQ( TIM5_IRQn );
+
+	__HAL_TIM_ENABLE_IT( &Timer5_16Khz, TIM_IT_UPDATE );
+
+//
+// Enable timer 5 update interrupt
+//
+	__HAL_TIM_ENABLE( &Timer5_16Khz );
+
+
+}
+
+int ConvertAudio(void)
+{
+	int
+		ADCResult;
+
+//
+// Start a conversion
+//
+	HAL_ADC_Start( &AudioAdc );
+
+//
+// Wait for end of conversion
+//
+    HAL_ADC_PollForConversion( &AudioAdc, HAL_MAX_DELAY );
+
+//
+// Get the 12 bit result
+//
+    ADCResult = HAL_ADC_GetValue( &AudioAdc );
+
+    return(ADCResult);
+}
+
+
+int ConvertReference(void)
+{
+	int
+		ADCResult;
+
+	ADC_ChannelConfTypeDef sConfig;
+
+//
+// Select the channel to convert and start the conversion
+//
+	sConfig.Channel = ADC_CHANNEL_12;
+	sConfig.Rank = 1;
+	sConfig.SamplingTime = ADC_SAMPLETIME_112CYCLES;
+	sConfig.Offset = 0;
+
+	HAL_ADC_ConfigChannel(&ReferenceAdc, &sConfig);
+
+//
+// Start a conversion
+//
+	HAL_ADC_Start( &ReferenceAdc );
+
+//
+// Wait for end of conversion
+//
+    HAL_ADC_PollForConversion( &ReferenceAdc, HAL_MAX_DELAY );
+
+//
+// Get the 12 bit result
+//
+    ADCResult = HAL_ADC_GetValue( &ReferenceAdc );
+
+    return(ADCResult);
 }
 
 #pragma GCC diagnostic pop
